e271de794b8744f2840017531876130d24fddd57
ljusas <ljusas@gmail.com>
2018-02-14 18:33:44 +0100

    Dodat sync u appbar

---
+++ Example12-master-b/app/src/main/java/rs/aleph/android/example12/activities/CategoryProvider.java
@@ -0,0 +1,41 @@
+package rs.aleph.android.example12.activities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ljubisa on 31.01.2018..
+ */
+
+public class CategoryProvider {
+
+    public static List<Category> getCategories() {
+
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category(0, "SIDE_DISH"));
+        categories.add(new Category(1, "MAIN_DISH"));
+        categories.add(new Category(2, "DESERT"));
+        return categories;
+    }
+
+    public static List<String> getCategoryNames() {
+        List<String> names = new ArrayList<>();
+        names.add("SIDE_DISH");
+        names.add("MAIN_DISH");
+        names.add("DESERT");
+        return names;
+    }
+
+    public static Category getCategoryById(int id) {
+        switch (id) {
+            case 0:
+                return new Category(0, "SIDE_DISH");
+            case 1:
+                return new Category(1, "MAIN_DISH");
+            case 2:
+                return new Category(2, "DESERT");
+            default:
+                return null;
+        }
+    }
+}
---
+++ Example12-master-b/app/src/main/java/rs/aleph/android/example12/async/SimpleSyncTask.java
@@ -0,0 +1,89 @@
+package rs.aleph.android.example12.async;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import rs.aleph.android.example12.R;
+import rs.aleph.android.example12.activities.MasterFragment;
+import rs.aleph.android.example12.activities.MealProvider;
+
+
+/**
+ * Created by milossimic on 10/22/16.
+ * AsyncTask klasa prima tri parametra prilikom specijalizacije
+ * Korisnici sami definisu tip u zavisnosti od posla koji zele da obave.
+ *
+ * Prvi argument predstavlja ulazne parametre, ono so zelimo
+ * da posaljemo zadatku. Recimo ime fajla koji zelimo da skinemo
+ *
+ * Drugi argument je indikator kako ce se meriti progres. Koliko je posla
+ * zavrseno i koliko je opsla ostalo.
+ *
+ * Treci parametar je povratna vrednost, tj sta ce metoda doInBackground
+ * vratiti kao poratnu vrednost metodi onPostExecute
+ */
+public class SimpleSyncTask extends AsyncTask<Void, Void, Void> {
+
+    private Activity activity;
+    private MasterFragment.OnItemSelectedListener listener;
+
+    public SimpleSyncTask(Activity activity) {
+        this.activity = activity;
+        listener = (MasterFragment.OnItemSelectedListener) activity;
+    }
+
+    /**
+     * Metoda se poziva pre samog starta pozadinskog zadatka
+     * Sve pripreme odraditi u ovoj metodi, ako ih ima.
+     */
+    @Override
+    protected void onPreExecute() {
+    }
+
+    /**
+     * Posao koji se odvija u pozadini, ne blokira glavnu nit aplikacije.
+     * Sav posao koji dugo traje izvrsavati unutar ove metode.
+     */
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            //simulacija posla koji se obavlja u pozadini i traje duze vreme
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     *
+     * Kada se posao koji se odvija u pozadini zavrsi, poziva se ova metoda
+     * Ako je potrebno osloboditi resurse ili obrisati elemente koji vise ne trebaju.
+     */
+    @Override
+    protected void onPostExecute(Void products) {
+
+        final List<String> mealNames = MealProvider.getMealNames();
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(activity, R.layout.list_item, mealNames);
+        ListView listView = (ListView) activity.findViewById(R.id.listofMeals);
+
+        listView.setAdapter(dataAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                listener.onItemSelected(position);
+            }
+        });
+
+        Toast.makeText(activity, "Sync done", Toast.LENGTH_SHORT).show();
+    }
+}
